922b71477d353386363ffc45054374002fdca81e
Arsh <arshphutela01@gmail.com>
2023-03-09 00:29:39 +0530

    Add files via upload

---
+++ Lesson 4 Challenge/Lesson 4 Challenge/Lesson_4_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_ChallengeApp.swift
+//  Lesson 4 Challenge
+//
+//  Created by Arsh on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
